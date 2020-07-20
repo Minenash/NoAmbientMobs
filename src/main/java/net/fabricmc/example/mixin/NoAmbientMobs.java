78e81e64e7e3bfae404516644dee00cb12424a28
Minenash <minenash@protonmail.com>
2020-07-19 22:55:50 -0400

    made it so ambient mobs don't automatically spawn

---
+++ src/main/java/net/fabricmc/example/mixin/NoAmbientMobs.java
@@ -0,0 +1,17 @@
+package net.fabricmc.example.mixin;
+
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Biome.class)
+public class NoAmbientMobs {
+	@Inject(method = "addSpawn", at = @At("HEAD"), cancellable = true)
+	private void addSpawn(SpawnGroup group, Biome.SpawnEntry spawnEntry, CallbackInfo info) {
+		if (group == SpawnGroup.AMBIENT)
+			info.cancel();
+	}
+}
