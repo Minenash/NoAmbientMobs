97c2971dc792b960e1ab7d73590cd7cdacb18338
Minenash <minenash@protonmail.com>
2020-08-10 22:11:58 -0400

    Updated to 1.16.2

--- src/main/java/com/minenash/noambientmobs/mixin/NoAmbientMobs.java
+++ src/main/java/com/minenash/noambientmobs/mixin/NoAmbientMobs.java
@@ -1,16 +1,16 @@
 package com.minenash.noambientmobs.mixin;
 
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.SpawnSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Biome.class)
+@Mixin(SpawnSettings.Builder.class)
 public class NoAmbientMobs {
-	@Inject(method = "addSpawn", at = @At("HEAD"), cancellable = true)
-	private void addSpawn(SpawnGroup group, Biome.SpawnEntry spawnEntry, CallbackInfo info) {
+	@Inject(method = "spawn", at = @At("HEAD"), cancellable = true)
+	private void addSpawn(SpawnGroup group, SpawnSettings.SpawnEntry spawnEntry, CallbackInfoReturnable<SpawnSettings.Builder> info) {
 		if (group == SpawnGroup.AMBIENT)
 			info.cancel();
 	}
